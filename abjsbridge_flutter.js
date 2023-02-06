07ef134a97a132bffc707bc9d4b5f54cd7d6b1be
kamleshsangani <37983541+kamleshsangani@users.noreply.github.com>
2023-02-06 21:38:52 +0400

    Adding Javascript bridge for Miniapp
    Adding Javascript bridge for Miniapp

--- abjsbridge_flutter.js
+++ abjsbridge_flutter.js
@@ -1,23 +1,35 @@
-            function fromFlutter(newTitle) {
-                document.getElementById("title").innerHTML = newTitle;
-             }
+function fromFlutter(newTitle) {
+    document.getElementById("title").innerHTML = newTitle;
+}
 
-            function selectedFrontImage(selectedImageAsBase64) {
-              document.getElementById("selectedImagePreview").src = "data:image/png;base64," + selectedImageAsBase64;
-             }
+function selectedFrontImage(selectedImageAsBase64) {
+    document.getElementById("selectedImagePreview").src = "data:image/png;base64," + selectedImageAsBase64;
+}
 
-             function launchCamera() {
-                cameraHandler.postMessage("selectedFrontImage");
-             }
+function launchCamera(callback) {
+    cameraHandler.postMessage(callback);
+}
 
-            function launchGallery() {
-                galleryHandler.postMessage("selectedFrontImage");
-             }
+function launchGallery() {
+    galleryHandler.postMessage(callback);
+}
 
-            function launchImageScan() {
-                scanHandler.postMessage("Hello from JS");
-             }
+function launchImageScan() {
+    scanHandler.postMessage("Hello from JS");
+}
 
-            function launchLivenessCheck() {
-                livenesseHandler.postMessage("Hello from JS");
-             }
+function launchLivenessCheck() {
+    livenesseHandler.postMessage("Hello from JS");
+}
+
+class ABJSBridge {
+    static call(functionName, requestBody, callbackName) {
+        var javaScriptMessage = {
+           body: requestBody,
+           callback: callbackName
+        }
+        if(functionName == "abSmeGetMobileAgent") {
+            mobileAgent.postMessage(JSON.stringify(javaScriptMessage));
+        }
+    }
+}
