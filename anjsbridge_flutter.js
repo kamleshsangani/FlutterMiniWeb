928a546d52262b532dfced243d7d9819f10cdc54
Benirex <benitorex.tataatsu@gmail.com>
2023-02-07 16:42:02 +0530

    camera function added

--- anjsbridge_flutter.js
+++ anjsbridge_flutter.js
@@ -2,9 +2,7 @@ function fromFlutter(newTitle) {
     document.getElementById("title").innerHTML = newTitle;
 }
 
-function selectedFrontImage(selectedImageAsBase64) {
-    document.getElementById("selectedImagePreview").src = "data:image/png;base64," + selectedImageAsBase64;
-}
+
 
 class ANJSBridge {
     static call(functionName, requestBody, callbackName) {
